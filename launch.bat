b62ef3a5adc56124016c6f6bb47c291b822f45a6
34j <55338215+34j@users.noreply.github.com>
2022-08-25 03:17:00 +0900

    Update launch.bat

--- launch.bat
+++ launch.bat
@@ -4,5 +4,5 @@ set PYTHONHOME=%LOCALAPPDATA%\Programs\Python\Python310
 set PATH=%PATH%;%PYTHONHOME%
 set RESOLVE_SCRIPT_API=%PROGRAMDATA%\Blackmagic Design\DaVinci Resolve\Support\Developer\Scripting
 set RESOLVE_SCRIPT_LIB=C:\Program Files\Blackmagic Design\DaVinci Resolve\fusionscript.dll
-set PYTHONPATH=%PYTHONPATH%;%PYTHONHOME%Lib\site-packages;%cd%\venv\Lib\site-packages;%RESOLVE_SCRIPT_API%\Modules
+set PYTHONPATH=%PYTHONPATH%;%PYTHONHOME%\Lib\site-packages;%cd%\venv\Lib\site-packages;%RESOLVE_SCRIPT_API%\Modules
 "C:\Program Files\Blackmagic Design\DaVinci Resolve\Resolve.exe"
